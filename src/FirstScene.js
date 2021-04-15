496a171d2e9f38d430440272be89bde63d1663ea
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-04-15 13:23:33 +0400

    h

--- src/FirstScene.js
+++ src/FirstScene.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from './images/logo.png'
 export default function FirstScene() {
     return (
-             <div className='first-scene hide'>
+             <div className='first-scene'>
                  <img id='first_screen' src={logo} alt='logo'/>
             </div>
     )
--- src/Website.js
+++ src/Website.js
@@ -10,7 +10,7 @@ export default function Website() {
 
  
     return (
-            <div className='website' id='home'>
+            <div className='website hide' id='home'>
                 <Navbar/>
                 <Header/>
                 <Story/>
