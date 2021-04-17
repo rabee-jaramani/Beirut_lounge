7dc61d40b7c37f5c5b01efe957a2bf4b06ae638c
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-04-17 22:29:48 +0400

    as

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
 export default function Header() {
-    setTimeout(() => {
-    document.querySelector('.header').classList.add('header-bg-changer')
+    // setTimeout(() => {
+    // document.querySelector('.header').classList.add('header-bg-changer')
         
-    }, 5000);
+    // }, 0);
     return (
         <>
-            <div className='header' id='home'>
+            <div className='header header-bg-changer' id='home'>
                 <div className='qoute'> 
                  <div>CAFE and RESTAURANT</div>
                 Our mission is to provide an unforgetable experience
